f3e8f5c3aa94863ceddf3ba24a2e7ff02263ef26
Masha Obaturova <m.obaturova@gmail.com>
2021-06-07 22:14:54 -0400

    added java version

--- GeoQuiz/java/app/src/main/java/com/mashmash/geoquiz/Question.java
+++ GeoQuiz/java/app/src/main/java/com/mashmash/geoquiz/Question.java
@@ -2,4 +2,12 @@ package com.mashmash.geoquiz;
 
 public class Question {
 
+    int textResId;
+    boolean answer;
+
+    public Question (int textResId, boolean answer) {
+        this.textResId = textResId;
+        this.answer = answer;
+    }
+
 }
