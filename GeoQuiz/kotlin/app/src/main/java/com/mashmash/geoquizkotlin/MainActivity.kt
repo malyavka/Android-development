fa54bb3f33beb8d643fea5d0267e0b9f7e85d8a3
Masha Obaturova <m.obaturova@gmail.com>
2021-06-07 22:14:26 -0400

    added kotlin version

--- GeoQuiz/kotlin/app/src/main/java/com/mashmash/geoquizkotlin/MainActivity.kt
+++ GeoQuiz/kotlin/app/src/main/java/com/mashmash/geoquizkotlin/MainActivity.kt
@@ -2,10 +2,39 @@ package com.mashmash.geoquizkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button_id)
+        falseButton = findViewById(R.id.false_button_id)
+        //Single Abstract method - SAM conversion
+        trueButton.setOnClickListener { }
+        trueButton.setOnClickListener {
+            val myToast = Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+            myToast.show()
+        }
+        falseButton.setOnClickListener {
+            val myToast = Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+            myToast.show()
+        }
     }
 }
